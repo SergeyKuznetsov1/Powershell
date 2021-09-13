c9644715fa639ae3ae41c3512070d1a3e15a0164
SergeyKuznetsov1 <39618322+SergeyKuznetsov1@users.noreply.github.com>
2021-09-13 18:01:53 +0300

    Add files via upload

---
+++ Civil2013.ps1
@@ -0,0 +1,3 @@
+﻿$result = cmd.exe /c D:\lmutil.exe lmstat -c "@SERVICESERVER" -f 85992IDSP_2013_0F
+[string]$UserName = $result -like "*start*"
+powershell (New-Object -ComObject Wscript.Shell).Popup($UserName ,0,"""A вот кто запустил Civil 2013""",0x0)
