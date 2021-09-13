ef5bbc88c106639963757fc46c0edec3c3d8b528
SergeyKuznetsov1 <39618322+SergeyKuznetsov1@users.noreply.github.com>
2021-09-13 17:15:53 +0300

    Add files via upload

---
+++ cert_to_log.ps1
@@ -0,0 +1,11 @@
+﻿# Get all certs from "MY" container and put them to log file in   
+
+
+$date = Get-Date -Format ddMMyyyy
+$Filename = $env:username +'_' + $date +'.log'
+
+$FilePath = '\\M-FS01-r1\Cert_Logs'+"`$"+"\logs\"
+
+$FilePath_full = $FilePath + $Filename
+
+$certs = gci Cert:\CurrentUser\My | format-list | Out-File -Force -FilePath $FilePath_full
