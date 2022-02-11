6b0d26fc7e9a622da88019eb7704d206f020d216
SergeyKuznetsov1 <39618322+SergeyKuznetsov1@users.noreply.github.com>
2022-02-11 13:56:28 +0300

    Add files via upload

--- copy_phone_to_ipphone.ps1
+++ copy_phone_to_ipphone.ps1
@@ -1,13 +1,10 @@
-﻿#Get-ADUser -Filter '(sAMAccountName -eq "u2610126")' -Properties l, physicalDeliveryOfficeName | ForEach-Object {Set-ADObject -Identity $_.DistinguishedName ` -Replace @{l=$($_.physicalDeliveryOfficeName)}}
-#$otherTelephone =  Get-ADUser -Filter '(sAMAccountName -eq "u2610126")' -Properties l, otherTelephone 
-#$otherTelephone
-
-
+﻿
 #Get-ADUser -Identity "U2610126" | Set-ADUser -ipPhone "34666"
 
-Get-ADUser -LDAPFilter '(&(!ipphone=*)(otherTelephone=*))' -Properties otherTelephone 
-
-| 
-      ForEach-Object{
-           Set-ADUser -Identity $_.SamAccountName -Add @{IpPhone=$_.otherTelephone}
-     }
+Get-ADUser -LDAPFilter '(&(!ipphone=*)(telephoneNumber=*))' -Properties telephoneNumber | 
+       ForEach-Object{
+            $number =  $_.telephoneNumber
+            $number = $number.Split(". ")[-1]
+            
+            Set-ADUser -Identity $_.SamAccountName -Add @{IpPhone=$number }
+      }
