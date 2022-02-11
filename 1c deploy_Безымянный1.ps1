684318b2a9e726301ed9139bc50660df840c54e3
SergeyKuznetsov1 <39618322+SergeyKuznetsov1@users.noreply.github.com>
2022-02-11 11:44:56 +0300

    Add files via upload

---
+++ 1c deploy_Безымянный1.ps1
@@ -0,0 +1,69 @@
+﻿
+#######
+# Get ProductCode from MSI file
+#######
+function Get-ProductCode ($pathMSI) {
+    $comObjWI = New-Object -ComObject WindowsInstaller.Installer
+    $MSIDatabase = $comObjWI.GetType().InvokeMember("OpenDatabase","InvokeMethod",$Null,$comObjWI,@($pathMSI,0))
+    $Query = "SELECT Value FROM Property WHERE Property = 'ProductCode'"
+    $View = $MSIDatabase.GetType().InvokeMember("OpenView","InvokeMethod",$null,$MSIDatabase,($Query))
+    $View.GetType().InvokeMember("Execute", "InvokeMethod", $null, $View, $null)
+    $Record = $View.GetType().InvokeMember("Fetch","InvokeMethod",$null,$View,$null)
+    $Value = $Record.GetType().InvokeMember("StringData","GetProperty",$null,$Record,1)
+    
+    Return $Value
+}
+
+
+######
+# Function to check if program with specified ProductCode already installed
+######
+function Check-Install($productCode) {
+    $registryPath32 = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$productCode"
+    $registryPath64 = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$productCode"
+    #$OS64bit = [Environment]::Is64BitOperatingSystem
+    $OS64bit = (Get-WmiObject  Win32_OperatingSystem).OSArchitecture
+    #Write-Host $registryPath
+    if ($OS64bit -like "64*") {
+        $installed = Test-Path -Path $registryPath64
+    } else {
+         $installed = Test-Path -Path $registryPath32
+    }
+    Return  $installed
+}
+
+######
+# Unstall MSI package
+######
+function Install-MSI ($pathMSI,$logFile) {
+        
+    &msiexec  /i $pathMSI   /qn TRANSFORMS="adminstallrelogon.mst;1049.mst" DESIGNERALLCLIENTS=1 THICKCLIENT=1 THINCLIENTFILE=1 THINCLIENT=1 WEBSERVEREXT=0 SERVER=0 CONFREPOSSERVER=0 CONVERTER77=0 SERVERCLIENT=0 LANGUAGES=RU /L*V+ $logFile | Out-Null
+}
+
+
+
+
+$targetVersion = "8.3.19.1229"      #change to your value
+$distribPath = "\\M-FS01-R1\1C_install$\1C_Client"           #change to your value
+$logPath = "\\M-FS01-R1\1C_install$\1C_Log"          #change to your value
+
+$pathMSI = "$distribPath\$targetVersion\1CEnterprise 8.msi"
+$pathMSI
+$hostname = hostname
+$logFile = "$logPath\$hostname.log"
+$datetime = Get-Date -Format "dd.MM.yyyy HH:mm:ss"
+
+$productCode = Get-ProductCode $pathMSI
+$productCode = $productCode[1]
+#Write-host $productCode | fl
+$installed = Check-Install $productCode
+
+if ($installed) {
+    "$datetime     1C:Предприятие $targetVersion ($productCode) already installed" | Out-File -FilePath "$logFile" -Append
+    } else {
+        "$datetime     Installing 1С:Предприятие $targetVersion ($productCode) ...." | Out-File -FilePath "$logFile" -Append
+        Install-MSI $pathMSI $logFile
+        $datetime_end = Get-Date -Format “dd.MM.yyyy HH:mm:ss”
+        "$datetime_end     Installation of 1С:Предприятие $targetVersion ($productCode) completed" | Out-File -FilePath "$logFile" -Append
+    }
+
---
+++ 1c deploy_Безымянный2.ps1
@@ -0,0 +1,8 @@
+﻿$string_to_convert = 'Турбанова Дарья Игоревна'
+$first_name = $string_to_convert.Split("")[1]
+$middle_name = $string_to_convert.Split(" ")[2]
+$surname = $string_to_convert.Split(" ")[0]
+
+$Givenname = $first_name +' '+ $middle_name
+
+$Givenname
